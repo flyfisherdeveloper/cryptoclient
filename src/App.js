35367b54f745c8b7fae2dcbebd6a9b3d2e76c263
Jeffrey Camomile <jeffrey.camomile@zionsbancorp.com>
2019-10-31 12:27:55 -0600

    Api call changes. Changed index.html

--- src/App.js
+++ src/App.js
@@ -12,15 +12,23 @@ class App extends Component {
       columnDefs: [{
         headerName: "Coin Pair", field: "symbol", sortable: true, filter: true
       }, {
-        headerName: "Base Asset", field: "baseAsset", sortable: true, filter: true
+        headerName: "Price Change", field: "priceChange", sortable: true, filter: true
       }, {
-        headerName: "Quote Asset", field: "quoteAsset", sortable: true, filter: true
+        headerName: "Price Change Percent", field: "priceChangePercent", sortable: true, filter: true
+      }, {
+        headerName: "High Price", field: "highPrice", sortable: true, filter: true
+      }, {
+        headerName: "Low Price", field: "lowPrice", sortable: true, filter: true
+      }, {
+        headerName: "Volume", field: "volume", sortable: true, filter: true
+      }, {
+        headerName: "Quote Volume", field: "quoteVolume", sortable: true, filter: true
       }]
     }
   }
 
   componentDidMount() {
-    fetch('http://localhost:8080/binance/info')
+    fetch('http://localhost:8080/api/v1/binance/24HourTicker')
         .then(result => result.json())
         .then(rowData => this.setState({rowData}))
   }
@@ -29,7 +37,7 @@ class App extends Component {
     return (
         <div
             className="ag-theme-balham"
-            style={{height: '200px', width: '600px'}}
+            style={{height: '200px'}}
         >
           <AgGridReact
               enableSorting={true}
